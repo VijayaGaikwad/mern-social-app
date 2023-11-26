1a3bd7e0caafe9a1283b801c6b23430ab9d3e81d
VijayaGaikwad <vijayag3097@gmail.com>
2023-11-26 12:12:25 +0530

    photos

--- Routes/UploadRoute.js
+++ Routes/UploadRoute.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "public/images");
+      cb(null, 'public/images/images.js/${images}');
     },
     filename: (req, file, cb) => {
       cb(null, req.body.name);
